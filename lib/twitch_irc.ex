68e7b78f287f7cf8d16b90485da51487034d2ffb
Norberts Kakste <norberts.kakste@gmail.com>
2019-02-03 20:36:04 +0200

    Removed debug functions

--- lib/twitch_irc.ex
+++ lib/twitch_irc.ex
@@ -1,22 +1,2 @@
 defmodule TwitchIrc do
-  defmodule ExampleConsumer do
-    use GenStage
-
-    def start_link(name) do
-      GenStage.start_link(__MODULE__, name)
-    end
-
-    def init(name) do
-      {:ok, file} = File.open("data.bin", [:write])
-      {:consumer, file, subscribe_to: [{name, max_demand: 100}]}
-    end
-
-    def handle_events(events, _from, file) do
-      for event <- events do
-        IO.binwrite(file, inspect(event) <> "\n")
-      end
-
-      {:noreply, [], file}
-    end
-  end
 end
